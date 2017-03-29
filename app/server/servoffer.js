caae882df103844b93944774e4efde5d6029fa34
sguo2017 <sguo08@qq.com>
2017-03-29 14:15:48 +0800

    serv-add view

--- app/server/servoffer.js
+++ app/server/servoffer.js
@@ -1,52 +1,24 @@
-/**
- * Created by ljunb on 16/8/21.
- */
 import React, {PureComponent} from 'react';
 import {
     View,
     Text,
     Image,
+    ProgressBarAndroid,
     TouchableOpacity,
+    TouchableHighlight,
     Platform,
     StyleSheet,
     Navigator
 } from 'react-native'
 import {observer} from 'mobx-react/native'
-import Login from '../pages/Login'
-import RootStore from '../mobx'
-import ScrollableTabView from 'react-native-scrollable-tab-view'
-import FeedsCategoryBar from '../components/FeedsCategoryBar'
-import FeedHomeListContainer from '../containers/feed/FeedHomeListContainer'
-// import FeedHomeList from '../../pages/feed/FeedHomeList';
-import FeedEvaluatingList from '../pages/feed/FeedEvaluatingList'
-import FeedKnowledgeList from '../pages/feed/FeedKnowledgeList';
-import FeedDelicacyList from '../pages/feed/FeedDelicacyList';
 import Header from '../components/HomeNavigation';
-const titles = ['首页', '评测', '知识', '美食'];
-const controllers = [
-    {categoryId: 1, controller: FeedHomeListContainer},
-    {categoryId: 2, controller: FeedEvaluatingList},
-    {categoryId: 3, controller: FeedKnowledgeList},
-    {categoryId: 4, controller: FeedDelicacyList}
-]
+import UselessTextInput from '../components/UselessTextInput';
 
 @observer
 export default class ServOffer extends PureComponent {
-
-    _pictureAction = () => {
-        const {user: {name}} = RootStore
-        if (name) {
-            alert(name)
-        } else {
-            this.props.navigator.push({
-                component: Login,
-                sceneConfig: Navigator.SceneConfigs.FloatFromBottom
-            })
-        }
-    }
-
+   
     render() {
-        const {navigator} = this.props;
+      
 
         return (
             <View style={{flex: 1}}>
@@ -54,36 +26,38 @@ export default class ServOffer extends PureComponent {
                     title='Sell a service'
                     leftIcon={require('../resource/t_header_arrow_left.png')}
                 />
-                <ScrollableTabView
-                    renderTabBar={() => <FeedsCategoryBar tabNames={titles}/>}
-                    tabBarPosition='top'
-                    scrollWithoutAnimation={false}
-                >
-                    {controllers.map((data, index) => {
-                        let Component = data.controller;
-                        return (
-                            <Component
-                                key={titles[index]}
-                                tabLabel={titles[index]}
-                                categoryId={data.categoryId}
-                                navigator={navigator}
-                            />
-                        )
-                    })}
-                </ScrollableTabView>
+                
+                <ProgressBarAndroid color="#60d795" styleAttr='Horizontal' progress={0.9} indeterminate={false} style={{marginTop:-10}}/>
+               
+                <Text style={{alignSelf:'flex-end', color:"#a8a6b9"}}>90%</Text>
+                
+                <Image style={{ width: 50,height: 50, alignSelf:'center'}} source={require('../resource/t_text.png')} />
+
+                <Text style={{alignSelf:'center', color:"#a8a6b9", fontSize: 12}}>What's the service?</Text>
+                
+                <Text style={{color:"#a8a6b9"}}>I will </Text>
+                
+                <UselessTextInput
+                      multiline = {true}
+                      numberOfLines = {3}
+                 />
+                
+                <View style={{alignItems: 'center',flexDirection: 'row'}}>
+                  <Text style={{color:"#a8a6b9"}}>min.15 characters</Text>
+                  <Text style={{alignSelf:'flex-end',right: 5, justifyContent: 'center', position: 'absolute', color:"#a8a6b9"}}>62</Text>
+                </View>
+                
+                <TouchableHighlight style={{backgroundColor: '#81d49c',marginTop: 20, alignSelf: 'stretch'}}>
+                  <Text style={{fontSize: 22, color: '#FFF', alignSelf: 'center', backgroundColor: '#81d49c',}}>
+                    下一步
+                  </Text>
+                </TouchableHighlight>
+
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    photo: {
-        width: gScreen.isIOS ? 44 : 50,
-        height: gScreen.isIOS ? 44 : 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        right: 0,
-        top: gScreen.navBarPaddingTop
-    }
+
 })
