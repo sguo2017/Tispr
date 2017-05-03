2804a02998485dfca93adb361936d390a8eb8c13
sguo2017 <sguo08@qq.com>
2017-05-03 10:46:45 +0800

    短信登录

--- app/user/login.js
+++ app/user/login.js
@@ -17,6 +17,26 @@ import TabBarView from '../containers/TabBarView';
 
 export default class Login extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      email: "p0@qq.com",
+      password: "123456",
+      error: "",
+      showProgress: false,
+      num: "",
+      code: "",
+    };
+  }
+
+  _navigateReg() {
+    this._navigate('Register');
+  }
+
+  _navigateHome() {
+    this._navigate('TabBarView');
+  }
+
   _navigate(routeName) {
     const { navigator } = this.props;
     if (routeName == 'Register') {
@@ -31,25 +51,6 @@ export default class Login extends Component {
         name: 'TabBarView'
       });
     }
-  }
-
-  _navigateReg() {
-    this._navigate('Register');
-  }
-
-  _navigateHome() {
-    this._navigate('TabBarView');
-  }
-
-  constructor() {
-    super();
-
-    this.state = {
-      email: "p2@qq.com",
-      password: "123456",
-      error: "",
-      showProgress: false,
-    };
   }
 
   async onLoginPressed() {
@@ -72,12 +73,12 @@ export default class Login extends Component {
       });
       let res = await response.text();
       let result = JSON.parse(res);
-      let userdetail=JSON.parse(result.user);
+      let userdetail = JSON.parse(result.user);
       if (response.status >= 200 && response.status < 300 && result.token) {
         UserDefaults.setObject(Constant.storeKeys.ACCESS_TOKEN_TISPR, result.token)
         let t = await UserDefaults.cachedObject(Constant.storeKeys.ACCESS_TOKEN_TISPR);
         console.log("79 accessToken:" + JSON.stringify(t));
-        global.user=userdetail;
+        global.user = userdetail;
         console.log(JSON.stringify(global.user))
         this._navigateHome();
       } else {
@@ -98,6 +99,90 @@ export default class Login extends Component {
       this.setState({ showProgress: false });
     }
   }
+
+  async _smsSend() {
+    try {
+      let url = 'http://' + Constant.url.SERV_API_ADDR + ':' + Constant.url.SERV_API_PORT + Constant.url.SERV_API_SMS_SEND_ADD;
+      let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          sms_send: {
+            recv_num: this.state.num,
+            sms_type: "code",
+          }
+        })
+      });
+      let res = await response.text();
+      let result = JSON.parse(res);
+      if (response.status >= 200 && response.status < 300) {
+
+      } else {
+        let error = res;
+        throw error;
+      }
+    } catch (error) {
+      this.setState({ error: error });
+      Alert.alert(
+        '提示',
+        '失败',
+        [
+          { text: '短信验证发送失败', onPress: () => console.log('确定') },
+        ]
+      )
+      this.setState({ showProgress: false });
+    }
+  }
+
+  async _smsCodeLogin() {
+    try {
+      let url = 'http://' + Constant.url.SERV_API_ADDR + ':' + Constant.url.SERV_API_PORT + Constant.url.SERV_API_USER_LOGIN;
+      let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            code: this.state.code,
+            num: this.state.num,
+          }
+        })
+      });
+      let res = await response.text();
+      let result = JSON.parse(res);
+      let userdetail = JSON.parse(result.user);
+      if (response.status >= 200 && response.status < 300 && result.token) {
+        UserDefaults.setObject(Constant.storeKeys.ACCESS_TOKEN_TISPR, result.token)
+        let t = await UserDefaults.cachedObject(Constant.storeKeys.ACCESS_TOKEN_TISPR);
+        console.log("79 accessToken:" + JSON.stringify(t));
+        global.user = userdetail;
+        console.log(JSON.stringify(global.user))
+        this._navigateHome();
+      } else {
+        UserDefaults.clearCachedObject(Constant.storeKeys.ACCESS_TOKEN_TISPR);
+        let error = res;
+        throw error;
+      }
+    } catch (error) {
+      this.setState({ error: error });
+      console.log("error " + error);
+      Alert.alert(
+        '提示',
+        '失败',
+        [
+          { text: '登录失败', onPress: () => console.log('确定') },
+        ]
+      )
+      this.setState({ showProgress: false });
+    }
+  }
+
+
 
   render() {
     return (
@@ -125,6 +210,28 @@ export default class Login extends Component {
         <TouchableHighlight onPress={this._navigateReg.bind(this)} style={styles.button}>
           <Text style={styles.buttonText}>
             注册
+          </Text>
+        </TouchableHighlight>
+
+        <TextInput
+          onChangeText={(text) => this.setState({ num: text })}
+          style={styles.input}
+          placeholder="手机号码">
+        </TextInput>
+        <TouchableHighlight onPress={this._smsSend.bind(this)} style={styles.button}>
+          <Text style={styles.buttonText}>
+            发送短信验证码
+          </Text>
+        </TouchableHighlight>
+
+        <TextInput
+          onChangeText={(text) => this.setState({ code: text })}
+          style={styles.input}
+          placeholder="验证码">
+        </TextInput>
+        <TouchableHighlight onPress={this._smsCodeLogin.bind(this)} style={styles.button}>
+          <Text style={styles.buttonText}>
+            短信验证码登录
           </Text>
         </TouchableHighlight>
 
