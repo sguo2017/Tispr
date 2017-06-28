d1d63e0876c43be8f6d22da82a0f5a4d40fa00bc
zengkm <zengkm3@mail2.sysu.edu.cn>
2017-06-28 16:40:42 +0800

    修改邮箱和密码

--- app/sys/account/changeMail.js
+++ app/sys/account/changeMail.js
@@ -6,14 +6,72 @@ import {
     TextInput,
     StyleSheet,
     Navigator,
-    TouchableHighlight
+    TouchableHighlight,
+    Alert
 } from 'react-native'
 import Header from '../../components/HomeNavigation';
 import accountSetting from './accountSetting';
+import Constant from '../../common/constants';
 export default class PasswordConfirm extends Component{
     constructor(props) {
 		super(props);
+        this.state = {
+            mail: ''
+        };
 	}
+
+    async _changeMail(){
+        try {
+            let url ='http://' + Constant.url.SERV_API_ADDR + ':' + Constant.url.SERV_API_PORT + Constant.url.SERV_API_USER_PASSWORD +'/' +global.user.id;            
+            let response = await fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user: {
+                        email: this.state.mail
+                    }
+                })
+            });
+            let res = await response.text();
+            let result = JSON.parse(res);
+            if (response.status >= 200 && response.status < 300) {
+               if (result.status && result.status == -1) {
+                   Alert.alert(
+                        '提示',
+                        '邮箱修改失败',
+                        [
+                        { text: '确定'},
+                        ]
+                    )              
+                }else if(result.status && result.status == -2){
+                    Alert.alert(
+                        '提示',
+                        '该邮箱已被注册',
+                        [
+                        { text: '确定'},
+                        ]
+                    ) 
+                }else{
+                    global.user.email = this.state.mail;
+                    Alert.alert(
+                        '提示',
+                        '邮箱修改成功',
+                        [
+                        { text: '确定', onPress: () => this.props.navigator.pop() },
+                        ]
+                    )  
+                }
+            } else {
+                let error = res;
+                throw error;
+            }
+        } catch (error) {
+            console.log("error "+error);
+        }
+    }
 
     render(){
         return(
@@ -29,13 +87,15 @@ export default class PasswordConfirm extends Component{
                     multiline={true}
                     numberOfLines={1}
                     placeholder='mail@examlple.com'
+                    value = {this.state.mail}
+                    onChangeText={(text) => this.setState({ mail: text })}
                     />
                 </View>
                 <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center',marginLeft:10 }}>
                     <Image style={{ height: 20, width: 30 }} source={require('../../resource/b_correct.png')} />
                     <Text style={{fontSize:16,color:global.gColors.themeColor}}>您现在使用的电子邮箱</Text>
                 </View>
-                <TouchableHighlight style={[styles.button, { backgroundColor: global.gColors.buttonColor, position: 'absolute', bottom:22, flexShrink: 0, width: global.gScreen.width }]}>
+                <TouchableHighlight onPress={this._changeMail.bind(this)} style={[styles.button, { backgroundColor: global.gColors.buttonColor, position: 'absolute', bottom:22, flexShrink: 0, width: global.gScreen.width }]}>
                     <Text style={styles.buttonText}>
                         确定
                     </Text>
--- app/sys/account/changePassword.js
+++ app/sys/account/changePassword.js
@@ -7,10 +7,12 @@ import {
     StyleSheet,
     Navigator,
     TouchableHighlight,
-    TouchableOpacity
+    TouchableOpacity,
+    Alert
 } from 'react-native'
 import Header from '../../components/HomeNavigation';
 import accountSetting from './accountSetting'
+import Constant from '../../common/constants';
 export default class PasswordConfirm extends Component{
     constructor(props) {
 		super(props);
@@ -20,6 +22,50 @@ export default class PasswordConfirm extends Component{
         }
 	}
     
+    async _changePassword(){
+        try {
+            let url ='http://' + Constant.url.SERV_API_ADDR + ':' + Constant.url.SERV_API_PORT + Constant.url.SERV_API_USER_PASSWORD +'/' +global.user.id;            
+            let response = await fetch(url, {
+                method: 'PATCH',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user: {
+                        password: this.state.password
+                    }
+                })
+            });
+             let res = await response.text();
+             let result = JSON.parse(res);
+            if (response.status >= 200 && response.status < 300) {
+               if (result.status && result.status==-1) {
+                    Alert.alert(
+                        '提示',
+                        '密码修改失败',
+                        [
+                        { text: '确定'},
+                        ]
+                    )                
+                }else{
+                    Alert.alert(
+                        '提示',
+                        '密码修改成功',
+                        [
+                        { text: '确定', onPress: () => this.props.navigator.pop()},
+                        ]
+                    ) 
+                }
+            } else {
+                let error = res;
+                throw error;
+            }
+        } catch (error) {
+            console.log("error "+error);
+        }
+    }
+
     render(){
         return(
             <View style={{height:global.gScreen.height}}>
@@ -33,6 +79,7 @@ export default class PasswordConfirm extends Component{
                     <View style={{flexDirection:'row'}}>
                         <TextInput
                             onChangeText={(text) => this.setState({ password: text })}
+                            value ={this.state.password}
                             style={[styles.input,{width:global.gScreen.width*0.7}]}
                             placeholder="请输入新密码"
                             secureTextEntry={this.state.seePassword}>
@@ -53,7 +100,7 @@ export default class PasswordConfirm extends Component{
                 <View style={{paddingLeft:53,paddingRight:20}}>
                     <Text>8至16个字符</Text>
                 </View>
-                <TouchableHighlight style={[styles.button, { backgroundColor: global.gColors.buttonColor, position: 'absolute', bottom:22, flexShrink: 0, width: global.gScreen.width }]}>
+                <TouchableHighlight onPress={this._changePassword.bind(this)} style={[styles.button, { backgroundColor: global.gColors.buttonColor, position: 'absolute', bottom:22, flexShrink: 0, width: global.gScreen.width }]}>
                     <Text style={styles.buttonText}>
                         确定
                     </Text>
