1a425e35628fb7c7713e1bb2a8dcf674da735e04
zengkm <zengkm3@mail2.sysu.edu.cn>
2017-07-05 17:55:18 +0800

    需求详情显示投标列表

--- app/buzz/HeadCard/CardDetail.js
+++ app/buzz/HeadCard/CardDetail.js
@@ -26,7 +26,9 @@ export default class CardDetail extends Component {
             dataSource: new ListView.DataSource({
                 rowHasChanged: (row1, row2) => row1 !== row2,
             }),
-            offerList: []
+            offerList: [],
+            bidderList: [],
+            bidderListLength: 0,
         }
     }
 
@@ -59,8 +61,12 @@ export default class CardDetail extends Component {
                 if (response.status == 200) return response.json()
                 return null
             }).then(responseData => {      
-                let bidderList;          
+                let bidderList = this.state.bidderList;          
                 bidderList = JSON.parse(responseData.feeds);
+                this.setState({
+                    bidderList: bidderList,
+                    bidderListLength: bidderList.length
+                })
             }).catch(error => {
                 console.log(`Fetch evaluating list error: ${error}`)             
             })
@@ -140,22 +146,24 @@ export default class CardDetail extends Component {
                             }/>: <View></View>
                         } 
                         <View style={{flexDirection: 'row', marginTop: 20, height: 48, justifyContent: 'space-between'}}>
-                            <Text style={{fontSize: 16, color: 'black'}}>投标&nbsp;&nbsp;&nbsp;4/5</Text>
-                            <TouchableOpacity style={{backgroundColor: '#4A90E2', borderRadius: 2, height: 28, width: 72}}>
+                            <Text style={{fontSize: 16, color: 'black'}}>投标&nbsp;&nbsp;&nbsp;{this.state.bidderListLength}/5</Text>
+                            <TouchableOpacity style={{backgroundColor: '#4A90E2', borderRadius: 2, height: Platform === 'ios'?35:28, width: 72}}>
                                 <Text style={{color: 'white', marginHorizontal: 8, marginVertical: 4}}>增加投标</Text>
                             </TouchableOpacity>
                         </View>                                                                    
                     </View>
-                    <ListView
-                        dataSource={this.state.dataSource.cloneWithRows(['row1', 'row2', 'row3', 'row4'])}
-                        renderRow={(rowData) => 
-                            <View style={{backgroundColor: '#FFFFFF', height: 64, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 16, paddingVertical: 12}}>
-                                <Image source={require('../../resource/user_default_image.png')} style={{borderRadius: 20, width: 40, height: 40}}></Image>
-                                <Text style={{color: '#1B2833', fontSize: 16, flexShrink: 0, marginLeft: 12}}>Frank Gardner</Text>
-                                <Image source={require('../../resource/g_chevron right.png')} style={{position: 'absolute', right: 16}}></Image>
-                            </View>
-                        }
-                    />
+                    {
+                        this.state.bidderList.map((data, index)=>{
+                            return(
+                                <View key={index} style={{backgroundColor: '#FFFFFF', height: 64, flexDirection: 'row', alignItems: 'center', paddingHorizontal: 16, paddingVertical: 12}}>
+                                    <Image source={{uri: data.offer_user_avatar}} style={{borderRadius: 20, width: 40, height: 40}}></Image>
+                                    <Text style={{color: '#1B2833', fontSize: 16, flexShrink: 0, marginLeft: 12}}>{data.offer_user}</Text>
+                                    <Image source={require('../../resource/g_chevron right.png')} style={{position: 'absolute', right: 16}}></Image>
+                                </View>
+                            ) 
+                        })
+                    }
+
                     <View style={{backgroundColor: 'white', paddingTop: 23, paddingBottom: 10}}>
                         <TouchableOpacity style={{backgroundColor: '#FFC400', borderRadius: 4, height: 44, marginHorizontal: 16, paddingHorizontal: 138, paddingVertical: 10}}
                             onPress={() => this._p(feed)}
