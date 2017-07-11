17b45cf3c7e11bf92a309e225052cff1868847fc
Kariko <925433739@qq.com>
2017-07-11 09:44:01 +0800

    举报页面

--- app/sys/others/report.js
+++ app/sys/others/report.js
@@ -7,15 +7,22 @@ import {
     StyleSheet,
     Navigator,
     TouchableHighlight,
-    Alert
+    Alert,
+    TouchableOpacity,
+    ScrollView
 } from 'react-native'
 import Header from '../../components/HomeNavigation';
 import Constant from '../../common/constants';
+import ImagePicker from 'react-native-image-picker';
+import AutoTextInput from '../../components/AutoTextInput';
+
 export default class PasswordConfirm extends Component{
     constructor(props) {
 		super(props);
         this.state = {
-            content: ''
+            content: '',
+            title: '',
+            choices: [false, false, false, false, false, false, false]
         };
 	}
 
@@ -62,25 +69,90 @@ export default class PasswordConfirm extends Component{
 					title='举报'
 					leftIcon={require('../../resource/ic_back_white.png')}
 					leftIconAction={()=> this.props.navigator.pop()}
+                    rightButton='提交'
+                    rightButtonAction={this._sendReport.bind(this)}
 				/>
-                <View style={{flexDirection:'row',justifyContent:'flex-start',alignItems:'center',margin:10}}>
-                    <TextInput
+                <ScrollView>
+                <View style={{marginHorizontal: 15, marginVertical: 25}}>
+                    <Text style={{color: '#999999', fontSize: 14}}>{this.state.title?this.state.title:'理由'}</Text>
+                </View>
+                <View style={{flexWrap: 'wrap', flexDirection: 'row', marginLeft: 5}}>
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[0] && { backgroundColor: global.gColors.themeColor}]}
+                        onPress={()=>this.setState({title: '虚假信息', choices: [true, false, false, false, false, false, false]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[0] && {color: 'white'}]}>虚假信息</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[1] && { backgroundColor: global.gColors.themeColor}]} 
+                        onPress={()=>this.setState({title: '泄露/侵犯他人隐私', choices: [false, true, false, false, false, false, false]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[1] && {color: 'white'}]}>泄露/侵犯他人隐私</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[2] && { backgroundColor: global.gColors.themeColor}]}
+                        onPress={()=>this.setState({title: '盗用他人作品', choices: [false, false, true, false, false, false, false]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[2] && {color: 'white'}]}>盗用他人作品</Text>
+                    </TouchableOpacity >
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[3] && { backgroundColor: global.gColors.themeColor}]} 
+                        onPress={()=>this.setState({title: '侮辱、谩骂、不文明用语', choices: [false, false, false, true, false, false, false]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[3] && {color: 'white'}]}>侮辱、谩骂、不文明用语</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[4] && { backgroundColor: global.gColors.themeColor}]} 
+                        onPress={()=>this.setState({title: '暴力、血腥、色情内容', choices: [false, false, false, false, true, false, false]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[4] && {color: 'white'}]}>暴力、血腥、色情内容</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[5] && { backgroundColor: global.gColors.themeColor}]} 
+                        onPress={()=>this.setState({title: '消极应对、拖延时间', choices: [false, false, false, false, false, true, false]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[5] && {color: 'white'}]}>消极应对、拖延时间</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={[styles.touch, this.state.choices[6] && { backgroundColor: global.gColors.themeColor}]} 
+                        onPress={()=>this.setState({title: '错误分类', choices: [false, false, false, false, false, false, true]})}
+                    >
+                        <Text style={[styles.text, this.state.choices[6] && {color: 'white'}]}>错误分类</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{marginHorizontal: 15, marginVertical: 25}}>
+                    <Text style={{color: '#999999', fontSize: 14}}>描述</Text>
+
+                    <AutoTextInput
+                        editable = {true}
+                        style={styles.innput}
+                        maxLength={1000}
+                        underlineColorAndroid="#F2F2F2"
+                        multiline={true}
+                        numberOfLines={0}
+                        value ={this.state.content}
+                        autoFocus
+                        onChangeText={(val) => {
+                        this.setState({ content: val})
+                        }}
+                    />
+
+                    {/*<TextInput
                     style={styles.input}
                     underlineColorAndroid="transparent"
                     multiline={true}
                     numberOfLines={1}
                     value = {this.state.content}
                     onChangeText={(text) => this.setState({ content: text })}
-                    />
+                    />*/}
+                    <View style={{alignItems: 'flex-end'}}>
+                        <Text style={{color: '#B8B8B8', fontSize: 12}}>{this.state.content? this.state.content.length:0}/1000</Text>
+                    </View>
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center',marginLeft:10 }}>
-                    <Text style={{fontSize:16,color:global.gColors.themeColor}}>请详细描述举报原因</Text>
+                <View style={{marginHorizontal: 15, marginVertical: 25}}>
+                    <Text style={{color: '#999999', fontSize: 14}}>附件</Text>
                 </View>
-                <TouchableHighlight onPress={this._sendReport.bind(this)} style={[styles.button, { backgroundColor: global.gColors.buttonColor, position: 'absolute', bottom:22, flexShrink: 0, width: global.gScreen.width }]}>
-                    <Text style={styles.buttonText}>
-                        确定
-                    </Text>
-                </TouchableHighlight>
+                </ScrollView>
             </View>
         )
     }
@@ -104,11 +176,24 @@ const styles = StyleSheet.create({
     height: 50,
     margin: 10,
     padding: 4,
-    fontSize: 18,
+    fontSize: 16,
     width: global.gScreen.width - 40,
     // borderWidth: 1,
     borderColor: '#48bbec',
     borderBottomWidth: 1,
     borderBottomColor: '#eeeeee',
-},
+  },
+  touch: {
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: '#4A90E2',
+    height: 28,
+    paddingHorizontal: 8,
+    marginLeft: 8,
+    marginBottom: 8
+  },
+  text: {
+      color: '#4A90E2',
+      lineHeight: 22
+  }
 })
